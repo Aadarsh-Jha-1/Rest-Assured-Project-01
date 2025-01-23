a57453b8d76b40722027123cb9c5b5093c6ebdc1
Aadarsh Jha <aadarshjhaofficials@gmail.com>
2025-01-23 10:16:11 +0530

    Added Update API Method In The Flow

--- src/test/java/googlePlaceAPI/Requests/EndToEndApiFlow.java
+++ src/test/java/googlePlaceAPI/Requests/EndToEndApiFlow.java
@@ -7,6 +7,11 @@ public class EndToEndApiFlow {
 	public static void main(String[] args) {
 		
 
+		String initialAddress = "29, side layout, cohen 09";
+		
+		String updatedAddress = "70 winter walk, USA";
+		
+		
 		String responseData = UtilityFunctions.AddPlaceAPI();
 		
 		String placeID = UtilityFunctions.getStringValueOf("place_id", responseData);
@@ -16,7 +21,13 @@ public class EndToEndApiFlow {
 		
 		// Add Others Methods 
 		
-		UtilityFunctions.GetPlaceAPI(placeID);
+		UtilityFunctions.GetPlaceAPI(placeID,initialAddress);
+		
+		UtilityFunctions.UpdatePlaceAPI(placeID);
+		
+		UtilityFunctions.GetPlaceAPI(placeID,updatedAddress);
+		
+		
 		
 	
 	}
