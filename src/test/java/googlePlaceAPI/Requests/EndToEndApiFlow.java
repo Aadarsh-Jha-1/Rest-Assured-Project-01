28199b94e3358fbd9ca52910a5b11f0fd6f3bd71
Aadarsh Jha <aadarshjhaofficials@gmail.com>
2025-01-24 10:39:26 +0530

    Complete End To End Flow

--- src/test/java/googlePlaceAPI/Requests/EndToEndApiFlow.java
+++ src/test/java/googlePlaceAPI/Requests/EndToEndApiFlow.java
@@ -7,29 +7,49 @@ public class EndToEndApiFlow {
 	public static void main(String[] args) {
 		
 
+		// Address Declaration
+		
 		String initialAddress = "29, side layout, cohen 09";
 		
 		String updatedAddress = "70 winter walk, USA";
 		
 		
+		// Add Place API
+		
 		String responseData = UtilityFunctions.AddPlaceAPI();
+		
+		
+		// Extract Place ID
 		
 		String placeID = UtilityFunctions.getStringValueOf("place_id", responseData);
 		
 		System.out.println("Place ID : "+placeID);
 		
-		
-		// Add Others Methods 
+
+		// Get Place API
 		
 		UtilityFunctions.GetPlaceAPI(placeID,initialAddress);
 		
+		
+		//Update Place API
+		
 		UtilityFunctions.UpdatePlaceAPI(placeID);
+		
+		
+		// Check Updated Address
 		
 		UtilityFunctions.GetPlaceAPI(placeID,updatedAddress);
 		
-		
-		
 	
+		//Delete Place API
+		
+		UtilityFunctions.DeletePlaceAPI(placeID);
+		
+		
+		// Confirm deletion Of Place
+		
+		UtilityFunctions.ConfirmDeletion(placeID);
+		
 	}
 
 }
