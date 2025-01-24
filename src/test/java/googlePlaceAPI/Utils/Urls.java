7cf0620d932033622fac007c840c3afc26b981bd
Aadarsh Jha <aadarshjhaofficials@gmail.com>
2025-01-24 10:42:16 +0530

    End point added for delete place API

--- src/test/java/googlePlaceAPI/Utils/Urls.java
+++ src/test/java/googlePlaceAPI/Utils/Urls.java
@@ -3,31 +3,63 @@ package googlePlaceAPI.Utils;
 public class Urls {
 
     
-	private static String baseUrl = "https://rahulshettyacademy.com";
+	
+		private static String baseUrl = "https://rahulshettyacademy.com";
 
-    public static String getBaseUrl() {
-        return baseUrl;
-    }
+		public static String getBaseUrl() 
+		
+		{
+        
+			return baseUrl;
+		
+		}
 	 
+		
     
+    	private static String addPlaceEndPoint = "maps/api/place/add/json";
 	 
-    private static String addPlaceEndPoint = "maps/api/place/add/json";
-	 
-	    public static String getAddPlaceEndPoint() {
-	        return addPlaceEndPoint;
+	    public static String getAddPlaceEndPoint() 
+	    
+	    {
+	    
+	    	return addPlaceEndPoint;
+	    
 	    }
+	    
 	    
 	    
 	    private static String getPlace = "maps/api/place/get/json";
 		 
-	    public static String getPlaceEndPoint() {
-	        return getPlace;
+	    public static String getPlaceEndPoint() 
+	    
+	    {
+	    
+	    	return getPlace;
+	    
 	    }
+	    
+	    
 	    
 	    private static String updatePlace = "maps/api/place/update/json";
 		 
-	    public static String updatePlaceEndPoint() {
-	        return updatePlace;
+	    public static String updatePlaceEndPoint() 
+	    
+	    {
+	    
+	    	return updatePlace;
+	    
 	    }
+	    
+	    
+	    
+	    private static String deletePlace = "maps/api/place/delete/json";
+
+		public static String deletePlaceEndPoint() 
+		
+		{
+		
+			return deletePlace;
+		
+		}
 
 }
